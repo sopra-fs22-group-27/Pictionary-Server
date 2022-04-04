d7cca0148138236b9352bfbc045393d7bb0e3561
Li <sky14566541@gmail.com>
2022-04-04 18:52:16 +0200

    add test files

---
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/service/UserServiceIntegrationTest.java
@@ -0,0 +1,78 @@
+package ch.uzh.ifi.hase.soprafs22.service;
+
+import ch.uzh.ifi.hase.soprafs22.constant.UserStatus;
+import ch.uzh.ifi.hase.soprafs22.entity.User;
+import ch.uzh.ifi.hase.soprafs22.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.server.ResponseStatusException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test class for the UserResource REST resource.
+ *
+ * @see UserService
+ */
+@WebAppConfiguration
+@SpringBootTest
+public class UserServiceIntegrationTest {
+
+  @Qualifier("userRepository")
+  @Autowired
+  private UserRepository userRepository;
+
+  @Autowired
+  private UserService userService;
+
+  @BeforeEach
+  public void setup() {
+    userRepository.deleteAll();
+  }
+
+  @Test
+  public void createUser_validInputs_success() {
+    // given
+    assertNull(userRepository.findByUsername("testUsername"));
+
+    User testUser = new User();
+    testUser.setPassword("testPassword");
+    testUser.setUsername("testUsername");
+    testUser.setEmail("email");
+    // when
+    User createdUser = userService.createUser(testUser);
+
+    // then
+    assertEquals(testUser.getId(), createdUser.getId());
+    assertEquals(testUser.getPassword(), createdUser.getPassword());
+    assertEquals(testUser.getUsername(), createdUser.getUsername());
+    assertEquals(testUser.getEmail(), createdUser.getEmail());
+    assertNotNull(createdUser.getToken());
+    assertEquals(UserStatus.ONLINE, createdUser.getStatus());
+  }
+
+  @Test
+  public void createUser_duplicateUsername_throwsException() {
+    assertNull(userRepository.findByUsername("testUsername"));
+
+    User testUser = new User();
+    testUser.setPassword("password1");
+    testUser.setUsername("testUsername");
+    testUser.setEmail("email1");
+    User createdUser = userService.createUser(testUser);
+
+    // attempt to create second user with same username
+    User testUser2 = new User();
+
+    // change the name but forget about the username
+    testUser2.setPassword("password2");
+    testUser2.setUsername("testUsername");
+    testUser2.setEmail("email2");
+    // check that an error is thrown
+    assertThrows(ResponseStatusException.class, () -> userService.createUser(testUser2));
+  }
+}
