70f847dd2900446334a65930d1ee428d5b2b2fb0
SyLi9527 <79486258+SyLi9527@users.noreply.github.com>
2022-05-07 16:21:54 +0200

    add some basic support for chatbox

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/rest/dto/MessageDTO.java
@@ -0,0 +1,13 @@
+public class MessageDTO {
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
