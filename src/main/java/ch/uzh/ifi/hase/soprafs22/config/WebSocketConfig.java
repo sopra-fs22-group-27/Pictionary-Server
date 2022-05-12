a3e107fb30e00ba7968da0f1a54064a2867ae586
SyLi9527 <79486258+SyLi9527@users.noreply.github.com>
2022-05-13 00:20:16 +0200

    specify listener for chatbox

--- src/main/java/ch/uzh/ifi/hase/soprafs22/config/WebSocketConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/config/WebSocketConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/chat").setAllowedOriginPatterns("https://localhost:8081/chat").withSockJS();
     }
 
     @Override
