f409d848ff04ae801661655c50751d9aacd74e58
Dylan Baumgartner <djb.21@live.com>
2022-05-28 12:05:35 +0200

    formatted reponse vision map

--- src/main/java/ch/uzh/ifi/hase/soprafs22/controller/VisionController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/controller/VisionController.java
@@ -7,6 +7,7 @@ import com.google.cloud.vision.v1.EntityAnnotation;
 import com.google.cloud.vision.v1.Feature.Type;
 import com.google.cloud.spring.vision.CloudVisionTemplate;
 
+import java.text.DecimalFormat;
 import java.util.Base64;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -17,12 +18,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 
 @RestController
@@ -64,9 +67,23 @@ public class VisionController {
                                         LinkedHashMap::new));
         // [END spring_vision_image_labelling]
 
-        map.addAttribute("annotations", imageLabels);
+        if(imageLabels.isEmpty()){
+           throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No classification was fetched vom Google Vision API");
+        }
+        DecimalFormat df = new DecimalFormat();
+        df.setMaximumFractionDigits(2);
+
+        Map<String, String> formatedMap = new LinkedHashMap<>();
+
+        for (Map.Entry<String, Float> entry : imageLabels.entrySet() ) {
+            double newFloat = Math.round(entry.getValue() * 100.0);
+            formatedMap.put(entry.getKey(), (int)newFloat + "%");
+        }
+
+        map.addAttribute("annotations", formatedMap);
         //map.addAttribute("gameID", gameToken);
         gameService.setMap(map);
+
         return ResponseEntity.ok(map);
     }
 
